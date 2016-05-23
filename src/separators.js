43a21a27694fa3d3945bda034c0ea2ccf3b5d419
fanfanqu <18829291202@163.com>
2016-05-23 17:39:29 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function thousands_separators(num) {
+	 var num= num.toString().split(".");
+  num[0] = num[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num.join(".");
   
 }
 
-module.exports = thousands_separators;
+ module.exports = thousands_separators;
+
